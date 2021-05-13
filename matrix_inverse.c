9d3eda8df607846a3a4921b48854caf8a374ff70
Grigory Pozdnyakov <gri279@gmail.com>
2021-05-14 02:08:16 +0300

    asd

--- matrix_inverse.c
+++ matrix_inverse.c
@@ -15,29 +15,28 @@ int col2process(int col, int shift, int n){
 
 int matrix_inverse(double *array, int n, double *inverse, int *vec, int shift, int rank, int total_size, double *row_buffer) {
     double eps = 0.00000001;
-    //int max_col = 0;
-    double max_element = 0;
+    int max_col = 0;
+    double max_element = array[0];
+    int proc = 0;
+    double  temp_el = 0;
     for(int i = 0; i < n; i++){
 MPI_Gather(array + i * shift, shift, MPI_DOUBLE, row_buffer, shift, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         /* move i'th row to row_buffer */
         if(rank == 0){
             for(int j = 0; j < n; j++){
                 if(row_buffer[j] > max_element){
-                    //max_col = j;
+                    max_col = j;
                     max_element = row_buffer[j];
-
                 }
-printf("%f ", row_buffer[j]);
             }
             if(fabs(max_element) < eps){
                 printf("matrix has det 0, sorry");
                 return -1;
             }
-	
         }
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Bcast(&max_element, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        printf("max_el %f", max_element);
+        max_col = col2process(max_col, shift, n);
         for(int j = 0; j < n; j++){
             for(int k = 0; k < shift; k++){
                 if(j != i){
@@ -45,11 +44,23 @@ printf("%f ", row_buffer[j]);
                 }
             }
         }
-/*
+        for(int k = 0; k < shift; k++){
+            if(rank == proc){
+                temp_el = array[k + shift * i];
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Bcast(&temp_el, 1, MPI_DOUBLE, proc, MPI_COMM_WORLD);
+            for(int j = 0; j < n; j++){
+                if(j != i){
+                    array[k + shift * j] -= temp_el * array[k + shift * i] / max_element;
+                }
+            }
+        }
+
         for(int k = 0; k < shift; k++){
             array[k + shift * i] /= max_element;
         }
-*/
+
         MPI_Barrier(MPI_COMM_WORLD);
 #if defined DEBUG
         if(rank == 0){
