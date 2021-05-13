bd9a521d7a93f573fc6b861bc312a6d395cbf104
Grigory Pozdnyakov <gri279@gmail.com>
2021-05-14 01:25:23 +0300

    asd
    as

--- matrix_inverse.c
+++ matrix_inverse.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <mpi.h>
 #include <stdio.h>
+#include "matrix_print.h>
 /* get process num by column */
 int col2process(int col, int shift, int n){
     int process = -1;
@@ -23,18 +24,18 @@ int matrix_inverse(double *array, int n, double *inverse, int *vec, int shift, i
             for(int j = 0; j < n; j++){
                 if(row_buffer[j] > max_element){
                     max_col = j;
-		    max_element = row_buffer[j];
-                    
+                    max_element = row_buffer[j];
+
                 }
             }
-		if(fabs(row_buffer[max_col]) < eps){
-                        printf("matrix has det 0, sorry");
-                        return -1;
-                    }
+            if(fabs(row_buffer[max_col]) < eps){
+                printf("matrix has det 0, sorry");
+                return -1;
+            }
         }
         MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Bcast(&max_element, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	printf("max_el %f", max_element);
+        MPI_Bcast(&max_element, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        printf("max_el %f", max_element);
         for(int j = 0; j < n; j++){
             for(int k = 0; k < shift; k++){
                 if(j != i){
@@ -46,7 +47,15 @@ int matrix_inverse(double *array, int n, double *inverse, int *vec, int shift, i
         for(int k = 0; k < shift; k++){
             array[k + shift * i] /= max_element;
         }
-MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+#if defined DEBUG
+        if(rank == 0){
+            printf("step: %i \n matrix: \n", i);
+        }
+
+        matrix_print(mat, n, m, 1, vec, shift, rank, total_size, row_buffer);
+        MPI_Barrier(MPI_COMM_WORLD);
+#endif
     }
     return 0;
 }
