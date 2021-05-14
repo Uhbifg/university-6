65c97b33f1f43904ddd826e96725cdd678bd242b
Uhbifg <gri279@gmail.com>
2021-05-14 05:05:10 +0300

    asds

--- matrix_inverse.c
+++ matrix_inverse.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include "matrix_print.h"
 /* get process num by column */
-int col2process(int col, int shift, int n){
+int col2process(int col, int shift){
     int process = -1;
     while(col >= 0){
         col -= shift;
@@ -15,11 +15,12 @@ int col2process(int col, int shift, int n){
 
 int matrix_inverse(double *array, int n, double *inverse, int *vec, int shift, int rank, int total_size, double *row_buffer, double *column_buffer) {
     double eps = 0.00000001;
-    int max_col = 0;
+   
+    for(int i = 0; i < n; i++){
+ int max_col = 0;
     double max_element = 0;
     int proc = 0;
-    double  temp_el = 0;
-    for(int i = 0; i < n; i++){
+
         MPI_Gather(array + i * shift, shift, MPI_DOUBLE, row_buffer, shift, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         /* move i'th row to row_buffer */
         if(rank == 0){
@@ -34,30 +35,38 @@ int matrix_inverse(double *array, int n, double *inverse, int *vec, int shift, i
                 printf("matrix has det 0, sorry");
                 return -1;
             }
+
         }
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Bcast(&max_element, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-
+	MPI_Bcast(&max_col, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 #if defined DEBUG
         if(rank == 0){
             printf("step: %i \n matrix: \n", i);
         }
-
         matrix_print(array, n, n, 1, vec, shift, rank, total_size, row_buffer);
+printf("max col = %d \n", max_col);
         MPI_Barrier(MPI_COMM_WORLD);
 #endif
-        proc = col2process(max_col, shift, n);
+        proc = col2process(max_col, shift);
         if(rank == proc){
             for(int j = 0; j < n; j++){
-                column_buffer[j] = array[i % shift + j * shift];
+                column_buffer[j] = array[max_col % shift + j * shift];
             }
         }
-        MPI_Bcast(column_buffer, n, MPI_DOUBLE, proc, MPI_COMM_WORLD);
-        for(int j = i + 1; j < n; j++){
+MPI_Bcast(column_buffer, n, MPI_DOUBLE, proc, MPI_COMM_WORLD);
+if(rank == 0){
+            for(int j = 0; j < n; j++){
+                printf("%f ", column_buffer[j]);
+            }
+        }
+        
+        for(int j = 0; j < n; j++){
+		if(i != j){
                 for (int k = 0; k < shift; k++) {
                     array[k + shift * j] -=  column_buffer[j] * array[k + shift * i] / max_element;
                 }
+}
         }
         for(int k = 0; k < shift; k++){
             array[k + shift * i] /= max_element;
